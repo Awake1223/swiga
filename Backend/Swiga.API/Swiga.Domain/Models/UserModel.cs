4553daeb8e8c6c6370f6569bb9d7cb571c1b5967
Awake1223 <zhora.sviridov.05@bk.ru>
2025-09-24 12:59:36 +0500

    Добавлены слои в серверную часть приложения. Добавлена модель в слой Domain - UserModel

---
+++ Backend/Swiga.API/Swiga.Domain/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swiga.Domain.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        
+    }
+}
